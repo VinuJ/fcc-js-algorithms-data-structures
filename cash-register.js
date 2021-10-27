694b46344ca0b23e1226d9be253f05ae2fb18a50
Vinu <severematrixoverload@gmail.com>
2021-10-27 15:23:27 +0100

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -83,5 +83,5 @@ function checkCashRegister(price, cash, cid) {
   return {status: "OPEN", change: [...finalChange]}
 }
   
-// Example: checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 // Should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
