b9312d3873fa9ec0f763d32a35d6e218f347dda1
Vinu <severematrixoverload@gmail.com>
2021-11-27 01:22:05 +0000

    Shortened

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -3,9 +3,5 @@ function palindrome(str) {
   let x = str.replace(/[^\w]|_/g, "").toUpperCase()
   
   // Check if string is equal to its split, reversed and rejoined form
-  if (x == x.split("").reverse().join("")) {
-    return true;
-  } else {
-    return false;
-  }
+  return (x == x.split("").reverse().join("")) ? true : false
 }
